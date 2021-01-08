14a3e9ab74d09653eeb842333b9aeb21a08be986
lsc99 <lsc990312@icloud.com>
2021-01-08 22:58:33 +0800

    新增了用于加速约束求解的类

---
+++ mcr-constraints/src/main/java/edu/tamu/aser/accelerate/AssertDetailInfo.java
@@ -0,0 +1,90 @@
+package edu.tamu.aser.accelerate;
+
+public class AssertDetailInfo {
+
+    // (or left right)
+    private String left;
+    private String right;
+
+    // AND 或者 OR
+    private String relation;
+
+    public AssertDetailInfo() {
+        left = right = "";
+    }
+
+    public static AssertDetailInfo getAssertDetailInfo(String ass) {
+        String[] lines = ass.split("\\s+");
+        AssertDetailInfo assertDetailInfo = new AssertDetailInfo();
+        int parentthesis = 0;
+        int rightParentthesis = 0;
+
+        if (lines[0].contains("and"))
+            assertDetailInfo.setRelation("AND");
+        else if (lines[0].contains("or"))
+            assertDetailInfo.setRelation("OR");
+        else
+            return null;//return ass;
+
+        int i = 1;
+        do {
+            String line = lines[i];
+
+            if (line.contains("("))
+                parentthesis++;
+            else if (line.contains(")"))
+                parentthesis--;
+
+            assertDetailInfo.addLeft(line + " ");
+
+            ++i;
+        } while (i < lines.length && parentthesis > 0);
+
+
+        do {
+            String line = lines[i];
+
+            if (line.contains("("))
+                parentthesis++;
+            else if (line.contains(")"))
+                parentthesis--;
+
+            assertDetailInfo.addRight(line + " ");
+
+            ++i;
+        } while (i < lines.length && parentthesis > 0);
+
+        assertDetailInfo.removeSpaces();
+
+        return assertDetailInfo;
+    }
+
+    public String getLeft() {
+        return left;
+    }
+
+    public void addLeft(String left) {
+        this.left += left;
+    }
+
+    public String getRight() {
+        return right;
+    }
+
+    public void addRight(String right) {
+        this.right += right;
+    }
+
+    public String getRelation() {
+        return relation;
+    }
+
+    public void setRelation(String relation) {
+        this.relation = relation;
+    }
+
+    public void removeSpaces() {
+        if (right.endsWith(" ")) right = right.substring(0, right.length() - 1);
+        if (left.endsWith(" ")) left = left.substring(0, left.length() - 1);
+    }
+}
---
+++ mcr-constraints/src/main/java/edu/tamu/aser/accelerate/MatchUnsatModel.java
@@ -0,0 +1,241 @@
+package edu.tamu.aser.accelerate;
+
+import java.util.*;
+
+public class MatchUnsatModel {
+    private static MatchUnsatModel instance = null;
+    // assert name - assert content 存储所有assert及其对编号，可能会比较多，后续可以优化
+    private final HashMap<String, String> assertContentToNameMap = new HashMap<>();
+    // assert content - assert name
+    private final HashMap<String, String> assertNameToContentMap = new HashMap<>();
+    // 全部的unsat-core
+    private final HashSet<String> unsatSet = new HashSet<>();
+    // 当前trace已命名的条件的名称集合
+    private final HashSet<String> curAssertNameSet = new HashSet<>();
+    // 当前trace的所有可能条件
+    private final ArrayList<ArrayList<String>> curAllConditionsList = new ArrayList<>();
+    private long index = 0L;
+    private int time = 1;
+
+    public static MatchUnsatModel getInstance() {
+        if (instance == null) {
+            instance = new MatchUnsatModel();
+        }
+
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        String line = "(and (< x10 x18 )  (and  (or (< x18 x13 ) (< x13 x10 ) ) (or (< x18 x5 ) (< x5 x10 ) ) ) )";
+
+        AssertDetailInfo assertDetailInfo = AssertDetailInfo.getAssertDetailInfo(line);
+        System.out.println(MatchUnsatModel.getInstance().namedComplexAssert(line));
+        System.out.println();
+        for (ArrayList<String> list : MatchUnsatModel.getInstance().curAllConditionsList) {
+            for (String s : list) System.out.print(s + " ");
+            System.out.println();
+
+        }
+        System.out.println();
+        MatchUnsatModel.getInstance().addUnsatCore("A0 A1 A4");
+        for (String s : MatchUnsatModel.getInstance().unsatSet) System.out.println(s);
+
+    }
+
+    /**
+     * 每此约束求解前初始化之前trace的所有记录的命名（z3语法要求）和所有条件的可能组合
+     */
+    public void initCurAssertNums() {
+        curAssertNameSet.clear();
+        curAllConditionsList.clear();
+    }
+
+    //
+
+    /**
+     * 添加unsat-core
+     *
+     * @param unsatCore z3求解后返回的unsat-core，例：A1 A3 A8
+     */
+    public void addUnsatCore(String unsatCore) {
+        //unsatSet.add(unsatCore.substring(1, unsatCore.length() - 1));
+        ArrayList<ArrayList<String>> unsatConditions = new ArrayList<>();
+
+        for (String assertName : unsatCore.split(" ")) {
+            String assertContent = assertNameToContentMap.get(assertName);
+            unsatConditions.add(getAssertAllPossibleCondition(assertContent));
+        }
+
+        unsatSet.addAll(getAssertsAllPossibleCondition(unsatConditions));
+    }
+
+    /**
+     * 检查当前trace的所有条件组合中是否包含已记录的unsat
+     *
+     * @return true表示需要进行z3求解
+     */
+    public boolean checkTraceUnsat() {
+        // 计算当前trace的条件的所有组合
+        ArrayList<String> traceAllPossibleCondition = getAssertsAllPossibleCondition(curAllConditionsList);
+
+        for (String traceCondition : traceAllPossibleCondition) {
+            // 一种组合的所有条件
+            List<String> conditions = Arrays.asList(traceCondition.split(" "));
+
+            for (String unsat : unsatSet) {
+                boolean isSat = false;
+
+                // 判断是否有unsat-core
+                for (String u : unsat.split(" ")) {
+                    if (!conditions.contains(u)) {
+                        isSat = true;
+                        break;
+                    }
+                }
+
+                //isSat值不变代表当前条件包含当前unsat-core的所有内容，即当前条件unsat，可以从trace的所有条件中删除当前条件
+                if (!isSat)
+                    traceAllPossibleCondition.remove(traceCondition);
+            }
+        }
+
+        return !traceAllPossibleCondition.isEmpty();
+    }
+
+    // 获得assert的命名
+    public String getAssertName(String assertContent) {
+        if (!assertContentToNameMap.containsKey(assertContent)) {
+            // 分别存储名称到内容和内容到名称的映射，方便读取
+            assertContentToNameMap.put(assertContent, "A" + index);
+            assertNameToContentMap.put("A" + index, assertContent);
+            index++;
+        }
+
+        return assertContentToNameMap.get(assertContent);
+    }
+
+    /**
+     * 添加当前条件中已存在的命名
+     * 如果已存在条件编号assertNum，则返回false；反之返回true
+     *
+     * @param assertNum
+     * @return true表示该命名在当前trace中无记录
+     */
+    public boolean addCurAssertNames(String assertNum) {
+        if (!curAssertNameSet.contains(assertNum)) {
+            curAssertNameSet.add(assertNum);
+            return true;
+        }
+
+        return false;
+    }
+
+    /***
+     * 处理简单的原始或者单一的or的条件语句，即为条件命名，例：(< x1 x2 )
+     * 同时记录条件的所有组合并且存储到{@link #curAllConditionsList}中
+     *
+     * @param simple
+     * @return 修改后的assert语句
+     */
+    public String namedSimpleAssert(String simple) {
+        String assertName = getAssertName(simple);
+
+        curAllConditionsList.add(getAssertAllPossibleCondition(simple));
+
+        if (addCurAssertNames(assertName))
+            return "(! " + simple + " :named " + assertName + " )";
+        else
+            return simple;
+    }
+
+    /***
+     * 将复杂的条件转换成简单的条件，即拆分and条件。同时分别对条件进行命名。or的条件整句命名，and的条件拆开后命名。
+     * 记录条件的所有组合操作再{@link #namedSimpleAssert}中完成
+     * @param complex
+     * @return 修改后的assert语句
+     */
+    public StringBuilder namedComplexAssert(String complex) {
+        StringBuilder result = new StringBuilder();
+
+        AssertDetailInfo assertDetailInfo = AssertDetailInfo.getAssertDetailInfo(complex);
+
+        String left = assertDetailInfo.getLeft();
+        String right = assertDetailInfo.getRight();
+        String relation = assertDetailInfo.getRelation();
+        AssertDetailInfo leftAssertDetailInfo = AssertDetailInfo.getAssertDetailInfo(left);
+        AssertDetailInfo rightAssertDetailInfo = AssertDetailInfo.getAssertDetailInfo(right);
+
+        if (relation == "AND") {
+            // and条件拆开后命名
+            if (leftAssertDetailInfo == null)
+                result.append("(assert " + namedSimpleAssert(left) + " )\n");
+            else
+                result.append(namedComplexAssert(left));
+
+            if (rightAssertDetailInfo == null)
+                result.append("(assert " + namedSimpleAssert(right) + " )\n");
+            else
+                result.append(namedComplexAssert(right));
+        } else if (relation == "OR") {
+            // or条件整句命名
+            result.append("(assert " + namedSimpleAssert(complex) + " )\n");
+        }
+
+
+        return result;
+    }
+
+    // 获得一个条件语句的所有可能组合
+    public ArrayList<String> getAssertAllPossibleCondition(String ass) {
+        ArrayList<String> result = new ArrayList<>();
+
+        AssertDetailInfo assertDetailInfo = AssertDetailInfo.getAssertDetailInfo(ass);
+
+        if (assertDetailInfo == null) { // ass是一个简单条件
+            result.add(getAssertName(ass)); // 获取名称并返回
+        } else { //ass是一个复杂条件
+            String left = assertDetailInfo.getLeft();
+            String right = assertDetailInfo.getRight();
+            String relation = assertDetailInfo.getRelation();
+
+            if (relation == "OR") { // or的组合
+                result.addAll(getAssertAllPossibleCondition(left));
+                result.addAll(getAssertAllPossibleCondition(right));
+            } else if (relation == "AND") { // and的组合
+                for (String l : getAssertAllPossibleCondition(left)) {
+                    for (String r : getAssertAllPossibleCondition(right)) {
+                        result.add(l + " " + r);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * @param conditionsList 需要计算的条件的组合的list
+     * @return 所有可能组合
+     */
+    public ArrayList<String> getAssertsAllPossibleCondition(ArrayList<ArrayList<String>> conditionsList) {
+        ArrayList<String> result = new ArrayList<>();
+
+        ArrayList<String> tmp = new ArrayList<>();
+        if (conditionsList.size() > 0) result.addAll(conditionsList.get(0));
+
+        for (int i = 1; i < conditionsList.size(); ++i) {
+            ArrayList<String> list = conditionsList.get(i);
+
+            for (String r : result)
+                for (String l : list)
+                    tmp.add(r + " " + l);
+
+            result = tmp;
+            tmp = new ArrayList<>();
+        }
+
+        return result;
+    }
+
+
+}
